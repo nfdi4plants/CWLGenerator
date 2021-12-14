95b68317116f60056767ba2f5f6ee33cf6a80890
Adrian Zimmer <z.adrian1995@gmail.com>
2021-12-14 17:03:56 +0100

    Add basic prompting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,52 @@
-import * as cwltsauto from 'cwl-ts-auto'
+import * as inquirer from 'inquirer'
 
-cwltsauto.loadDocument('resources/test.cwl').then((loadedDocument) => {console.log(loadedDocument)})
+console.log('CWLGenerator!')
+let allanswers: any[] = []
+
+async function generateInputs(count: number) {
+    let allInputs = []
+    for(let i = 0; i < count; i++) {
+        console.log(`Configuring Input ${i+1}`)
+        let answers = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'inputname',
+                message: "What is the name of the input?",
+            },
+            {
+                type: 'confirm',
+                name: 'isArray',
+                message: "Is your input a list of things?",
+            },
+            {
+                type: 'list',
+                name: 'inputType',
+                message: "What is the type of the input?",
+                choices: ["File", "Directory", "Number"]
+            },
+        ])
+        allInputs.push(answers)
+    }
+    return allInputs
+}
+
+async function main() {
+    let answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'basecommand',
+            message: "What is the basecommand to run your tool (e.g. samtools view)",
+        }, {
+            type: 'input',
+            name: 'inputCount',
+            message: "How many inputs does your tool have?",
+        },
+    ])
+
+    let inputs = await generateInputs(answers.inputCount)
+
+    return inputs
+}
+
+
+main().then((result)=> {console.log(result)})

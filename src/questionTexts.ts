916bc81bd4689159e15a9a80a339d46a50d8443c
Joott <jonathan.ott1994@gmail.com>
2021-12-16 12:10:08 +0100

    simplify questions

--- src/questionTexts.ts
+++ src/questionTexts.ts
@@ -5,7 +5,7 @@ export const inputCountText =
     "How many inputs does your tool have?"
 
 export const inputNameText =
-    "What is the name of the input (e.g. threads)?"
+    "What is the name you want to assign to the input (e.g. threads)?"
 
 export const inputPrefixText =
     "What is the command prefix for your input (e.g. -t). Press Enter if there is none."
@@ -19,20 +19,16 @@ export const inputTypeText =
 
 export const outputLocationKnownText =
     "Do you know the location of your output? The location is required as a Glob pattern.\n"
-    + "Examples:\n"
-    + "- An output directory in the working directory: /myOutputDir\n"
-    + "- An output directory in a subfolder of the working directory: /*/myOutputDir\n"
-    + "- Files of the same type in the working directory: /*.txt\n"
-    + "- Files of the same type in a subfolder of the working directory: /*/*.txt\n"
-    + "- If your output directory is specified under inputs: /$(inputs.NameOfTheInput)\n"
+    + "You will be asked to specify it later for each output.\n"
     + "If the location is unknown, the entire working directory will be returned."
 
 export const outputCountText =
-    "How many outputs does your tool have?"
+    "How many outputs does your tool have?\n"
+    + "A result folder for example is a single output."
 
 export const outputNameText = 
-    "What is the name of the output (e.g. results)?\n"
-    + "Warning: Do not repeat a name already given for an input."
+    "What is the name you want to assign to the output (e.g. results)?\n"
+    + "Warning: Do not repeat a name already assigned for an input."
 
 export const outputIsArrayText =
     "Does your output consist of multiple elements (e.g. a list of files or directories)"
